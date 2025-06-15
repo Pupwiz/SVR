c2e74e14434812c396dd38eb03a3326495576fc4
Pupwiz <pupwiz@gmail.com>
2025-06-15 07:55:29 -0400

    Add files via upload
    add to default install

---
+++ minimalxfce4.sh
@@ -0,0 +1,18 @@
+sudo apt install -y xserver-xorg-core \
+xserver-xorg-video-nvidia \
+x11-xserver-utils \
+x11-xkb-utils \
+x11-utils \
+thunar \
+xfce4-panel \
+xfce4-session \
+xfce4-settings \
+xfce4-terminal \
+xfconf \
+xfdesktop4 \
+xfwm4 \
+network-manager-openvpn \
+network-manager-gnome \
+network-manager-openvpn-gnome \
+xinit \
+xrdp
